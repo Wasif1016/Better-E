6b968af34694620aee6a5193ddfd11ef5c58551d
wasif <wasifdevelopsite@gmail.com>
2025-10-05 07:57:20 -0700

    test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -116,7 +116,7 @@ const Header = () => {
                 <input
                   type="text"
                   id="preorder-name"
-                  className="w-full px-3  py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-accent"
                   placeholder="Uw naam"
                 />
               </div>
